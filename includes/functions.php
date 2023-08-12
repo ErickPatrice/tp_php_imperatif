19c6bfee7758511d8eacff8069e3a47eb6b9931c
ErickChezOCs <nounga72@yahoo.fr>
2023-08-12 21:56:56 +0200

    gestion_des_membres_première_partie_OK

---
+++ includes/functions.php
@@ -0,0 +1,57 @@
+
+<?php
+
+// ... (autres fonctions existantes) ...
+
+// Ajouter un membre
+function addMember($nom, $prenom, $email, $password) {
+    $members = getAllMembers();
+    foreach ($members as $member) {
+        if ($member['email'] === $email) {
+            return false; // Membre avec la même adresse email existe déjà
+        }
+    }
+    $newMember = array(
+        'nom' => $nom,
+        'prenom' => $prenom,
+        'email' => $email,
+        'password' => $password
+    );
+    $members[] = $newMember;
+    saveMembersToFile($members);
+    return true;
+}
+
+// Supprimer un membre par email
+function deleteMember($email) {
+    $members = getAllMembers();
+    foreach ($members as $key => $member) {
+        if ($member['email'] === $email) {
+            unset($members[$key]);
+            $members = array_values($members);
+            saveMembersToFile($members);
+            return true;
+        }
+    }
+    return false;
+}
+
+// Mettre à jour les informations d'un membre
+function updateMember($email, $newNom, $newPrenom) {
+    $members = getAllMembers();
+    foreach ($members as &$member) {
+        if ($member['email'] === $email) {
+            $member['nom'] = $newNom;
+            $member['prenom'] = $newPrenom;
+            saveMembersToFile($members);
+            return true;
+        }
+    }
+    return false;
+}
+
+
+?>
+
+
+ 
---
+++ pages/login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <?php include 'templates/header.php'; ?>
+        </header>
+
+<main>
+    <h1>Connexion</h1>
+    <?php
+    // Vérifier si le formulaire a été soumis
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        // Vérifier les informations d'authentification
+        if (authenticateMember($email, $password)) {
+            $_SESSION["member_authenticated"] = true;
+            header("Location: les_membres.php");
+            exit();
+        } else {
+            echo "<p class='text-danger'>Identifiants incorrects. Veuillez réessayer.</p>";
+        }
+    }
+    ?>
+    <form method="post">
+        <div class="mb-3">
+            <label for="email" class="form-label">Adresse Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de Passe</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Se Connecter</button>
+    </form>
+</main>
+
+
+
+
+        <footer>
+            <?php include 'templates/footer.php'; ?>
+        </footer>
+    </div>
+</body>
+</html>
