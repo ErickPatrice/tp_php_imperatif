403de6ee522f8e5d0d1a4c843ec06650ad4dcc40
ErickChezOCs <nounga72@yahoo.fr>
2023-08-13 07:05:10 +0200

    remise à plat

--- templates/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div class="container">
-        <p class="footer-text">&copy; <?php echo date("Y"); ?> Association La Belle Affaire </br>
-        2 Avenue des champs Elysées </br> 75000 Paris. Tous droits réservés.</p>
-    </div>
-</footer>
